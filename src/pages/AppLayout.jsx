269f15f9846699fea8caaa15b1ea993d13ae47fc
zeegerrank <zeegerrank@gmail.com>
2024-10-14 22:38:26 +0700

    fix app height

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,20 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/Footer";
-import { twMerge } from "tailwind-merge";
 import BlockProvider from "../context/BlockProvider";
 
 function AppLayout() {
   return (
     <BlockProvider>
-      <div
-        className={twMerge(
-          "h-screen w-screen",
-          "transition-all duration-500",
-          "bg-complementary-100 text-accent-500",
-          "dark:bg-accent-900 dark:text-complementary-100",
-        )}
-      >
+      <div>
         <Header />
         <main>
           <Outlet />
